2edb90cd2a01953a1a398fe6358ec48d1ce897dd
devjjinii <devjjinii@naver.com>
2020-10-01 14:58:33 +0900

    update category crawl

--- ecommerce/spiders/gmarket_category_all.py
+++ ecommerce/spiders/gmarket_category_all.py
@@ -15,7 +15,46 @@ class GmarketCategoryAllSpider(scrapy.Spider):
         category_names = response.css('div.gbest-cate ul.by-group li > a::text').getall()
         for index, category_link in enumerate(category_links):
             # print(category_link, category_names[index])
-            print('http://corners.gmarket.co.kr' + category_link, category_names[index])
+            # print('http://corners.gmarket.co.kr' + category_link, category_names[index])
+            yield scrapy.Request(url='http://corners.gmarket.co.kr' + category_link, callback=self.parse_items, meta={'maincategory_name':
+                category_names[index]})
+
+        for index, category_link in enumerate(category_links):
+            yield scrapy.Request(url='http://corners.gmarket.co.kr' + category_link, callback=self.parse_subcategory, meta={'maincategory_name':
+                category_names[index]})
+   
+    def parse_subcategory(self, response):
+        print('parse_subcategory', response.meta['maincategory_name'])
+        subcategory_links = response.css('div.navi.group > ul > li > a::attr(href)').getall()
+        subcategory_names = response.css('div.navi.group > ul > li > a::text').getall()
+        for index, subcategory_link in enumerate(subcategory_links):
+            # print('http://corners.gmarket.co.kr' + subcategory_link, subcategory_names[index])
+            yield scrapy.Request(url='http://corners.gmarket.co.kr' + subcategory_link, callback=self.parse_items, meta={'maincategory_name':
+                response.meta['maincategory_name'], 'subcategory_name':subcategory_names[index] })
+
+
+    def parse_items(self, response):
+        print('parse_maincategory', response.meta['maincategory_name'])
+
+        best_items = response.css('div.best-list')
+        for index, item in enumerate(best_items[1].css('li')):
+            ranking = index + 1
+            title = item.css('a.itemname::text').get()
+            ori_price = item.css('div.o-price::text').get()
+            dis_price = item.css('div.s-price strong span span::text').get()
+            discount_percent = item.css('div.s-price em::text').get()
+           
+            if ori_price == None:
+                ori_price = dis_price
+                ori_price = ori_price.replace(",", "").replace("원", "")
+                dis_price = dis_price.replace(",", "").replace("원", "")
+            if discount_percent == None:
+                discount_percent = '0'
+            else:
+               discount_percent = discount_percent.replace("%", "")
+       
+            # print(ranking, title, ori_price, dis_price, discount_percent)
 
     # def parse_sub(self, response):
     #     pass
+
